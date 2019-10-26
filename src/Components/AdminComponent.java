6895c30697c0f011e84e611232fecedf90ce4131
Rajasekhar Reddy <rajasekharchanti5599@gmail.com>
2019-10-26 13:14:05 -0700

    Admin Component

---
+++ src/Components/AdminComponent.java
@@ -0,0 +1,35 @@
+package servlet;
+
+public class AdminComponent {
+
+		private String FirstName;
+		private String Email;
+		
+		
+		public String getFirstName() {
+			return FirstName;
+		}
+		/**
+		 * @param userId the userId to set
+		 * @param ClerkID 
+		 */
+		public void setFirstName(String FirstName) {
+			this.FirstName = FirstName;
+		}
+		/**
+		 * @return the email
+		 */
+		public String getEmail() {
+			return Email;
+		}
+		/**
+		 * @param email the email to set
+		 */
+		public void setEmail(String Email)
+		{
+			this.Email = Email;
+		}
+		
+		} 
+
+
