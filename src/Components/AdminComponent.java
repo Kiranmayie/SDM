52d7ddcf43f084a0179de2f88e5ae93ac561ab4e
Kiranmayie <47037511+Kiranmayie@users.noreply.github.com>
2019-10-27 13:15:23 -0400

    Updated

--- src/Components/AdminComponent.java
+++ src/Components/AdminComponent.java
@@ -1,4 +1,4 @@
-package servlet;
+package Components;
 
 public class AdminComponent {
 
--- src/databaseconnection/RentalMain.java
+++ src/databaseconnection/RentalMain.java
@@ -7,6 +7,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import Components.AdminComponent;
 import Components.ClerkComponent;
 import Components.ClientComponent;
 public class RentalMain {
